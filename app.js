8c42d87f7a944e1f3a8ed7174a4f3b514eb4615e
Ben Lenton <benlenton@gmail.com>
2022-01-24 18:32:13 +0000

    created basic models, linked to database, refactored resolvers to work with MongoDB

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");  // exports valid middleware functions; takes incoming requests and funnels them through the GraphQL query parser
 const { buildSchema } = require("graphql");  // object destructuring - pull out the buildSchema property from "graphql" (function to define GraphQL schema using template literals)
+const mongoose = require("mongoose");
+const Event = require("./models/event");
 
 // calls express imported from the express package to create an app object to use to start the node server etc
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
 // define API endpoint and middleware (pass in javascript object to configure our GraphQL API)
 app.use(
-    "/graphql", 
+    "/graphql",
     graphqlHTTP({
         schema: buildSchema(`
             type Event {
@@ -45,23 +45,42 @@ app.use(
         `),
         // resolvers for above schema (must have identical names):
         rootValue: {
-            events: () => {
-                return events;
+            events: async () => {
+                try {
+                    // When not re-saving a queried doc, .lean() avoids mongoose hydrating the doc
+                    return await Event.find().lean();
+                } catch (err) {
+                    throw err;
+                }
             },
             createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
+                const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: args.eventInput.date
-                };
-                events.push(event);
-                return event;
+                    date: new Date(args.eventInput.date)
+                })
+                return event
+                    .save()
+                    .then(result => {
+                        console.log(result);
+                        return result;
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        throw err;
+                    });
             }
         },
         graphiql: true
     })
 );
 
-app.listen(3000);
+// connecting app to database (using Mongoose and MongoDB Atlas)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ppjrt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
---
+++ models/event.js
@@ -0,0 +1,27 @@
+/**
+ * A JavaScript model created with the mongoose package, which uses models to manager data (easier to implement a model controller endpoint)
+ */
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;  // schema property (points at a constructor function - used to generate new schema objects)
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+});
+//                          (model name, schema to be used) 
+module.exports = mongoose.model("Event", eventSchema)
