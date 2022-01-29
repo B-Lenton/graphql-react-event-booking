4f381067f305ff1749b57e044eeb198dec19d741
Ben Lenton <benlenton@gmail.com>
2022-01-29 17:14:56 +0000

    created react navbar and bare bones of the frontend (react-router-dom@4.3.1 - to be upgraded & refactored as an additional challenge)

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+import AuthPage from "./pages/Auth";
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
+// below must be pascal case to be used in JSX (but doesn't have to be in original file) 
+import MainNavigation from "./components/Navigation/MainNavigation";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
